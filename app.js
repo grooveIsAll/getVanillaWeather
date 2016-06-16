5522af169ba35d54da0c0411c6a4519f3b8a3e52
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-16 14:46:43 -0700

    [feat]: render UI elements w/ API data

--- app.js
+++ app.js
@@ -24,7 +24,15 @@ app.client.get("https://weathersync.herokuapp.com/ip", function(response) {
   let weatherData = new app.HttpClient()
     .get(weatherUrl, function(response) {
       let data = JSON.parse(response);
-      console.log(data);
+      
+      let weatherIcon = data.weather[0].icon;
+      let iconUrl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+
+      let location = document.getElementById("loc").innerHTML = data.name;
+      let temperature = document.getElementById("temp").innerHTML = data.main.temp;
+      let img = document.getElementById("icon").src = iconUrl;
+      let condition = document.getElementById("cond").innerHTML = data.weather[0].main;
+
     });
 });
 
